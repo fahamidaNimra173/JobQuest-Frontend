0b4280647170534e66636059d9aa7801dc34c46b
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-10-03 19:03:26 +0600

    setup candidate and employer signup route and login route

---
+++ src/app/(auth)/signup/candidate/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CandidateSignUp = () => {
+    return (
+        <div>
+            CandidateSignUp
+        </div>
+    );
+};
+
+export default CandidateSignUp;
---
+++ src/app/(auth)/signup/employer/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmployerSignUp = () => {
+    return (
+        <div>
+            EmployerSignUp
+        </div>
+    );
+};
+
+export default EmployerSignUp;
